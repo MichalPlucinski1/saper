1936ae1c0a4dc74b0c72b5454faeba6f09181955
Pluto <mic.plu@wp.pl>
2022-11-21 22:11:58 +0100

    Working table show and refresh

--- saper.cpp
+++ saper.cpp
@@ -2,6 +2,10 @@
 //
 
 #include <iostream>
+#include "ClearScreen.h"
+#include<conio.h>
+#include <stdio.h>
+#include <string.h>
 using namespace std;
 
 
@@ -12,17 +16,23 @@ public:
     bool selected;
     bool hidden;
     bool bomb;
+    static bool lost;
     Cell();
     void fillCell(int,int);
     static void showTable();
     void showCell();
 };
 
+
 Cell::Cell()
 {
     hidden = true;
     selected = false;
 }
+
+bool Cell::lost = false;
+Cell cell[5][5];
+
 
 void Cell::fillCell(int _x, int _y)
 {
@@ -33,10 +43,21 @@ void Cell::fillCell(int _x, int _y)
 
 void Cell::showCell()
 {
-    if (this->selected)
+    if (this->selected && this->hidden)
+        cout << "[( )]";
+    else if (this->selected && !(this->hidden))
     {
-        cout <<"[()]"
+        if (this->bomb)
+        {
+            cout << endl << endl << "You lost!";
+            getchar();
+        }
+        else cout << "[(X)]";
     }
+    else if (!(this->selected) && this->hidden)
+        cout << "[   ]";
+    else
+        cout << "[ X ]";
 }
 
 void Cell::showTable()
@@ -47,20 +68,29 @@ void Cell::showTable()
         {
             cell[i][j].showCell();
         }
+        cout << endl;
     }
 }
 
-Cell cell[5][5];
-cell[0][0]->selected
+
+
 
 int main()
 {
-    
+    cell[0][0].selected = true;
     for (int i = 0; i < 5; i++)
     {
         for (int j = 0; j < 5; j++)
         {
             cell[i][j].fillCell(i, j);
         }
+    }
+
+
+    while (getchar() != 'q')
+    {
+        ClearScreen();
+        Cell::showTable();
+        
     }
 }
